6396bcbd1065a053d270940dce16aa5483eed190
André Morsch <andremorsch37@gmail.com>
2024-01-08 20:26:37 -0300

    feat: Inicia criacao da entidade Person

---
+++ LarTechAPi.Domain/Entities/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LarTechAPi.Domain.Entities
+{
+    public sealed class Person
+    {
+        public Person(string name, DateTime birthday, bool isActive)
+        {
+            Name = name;
+            Birthday = birthday;
+            IsActive = isActive;
+        }
+
+        public Person(int id, string name, DateTime birthday, bool isActive)
+        {
+            Id = id;
+            Name = name;
+            Birthday = birthday;
+            IsActive = isActive;
+        }
+
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public DateTime Birthday { get; private set; }
+        public bool IsActive { get; private set; }
+
+        public void StatusUpdate(bool isActive)
+        {
+            IsActive = isActive;
+        }
+    }
+}
