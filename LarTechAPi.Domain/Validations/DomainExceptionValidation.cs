5c67b4214bfc5807c65f8d6f2118c9664fd55c56
André Morsch <andremorsch37@gmail.com>
2024-01-08 20:27:44 -0300

    Update DomainExceptionValidation.cs

--- LarTechAPi.Domain/Validations/DomainExceptionValidation.cs
+++ LarTechAPi.Domain/Validations/DomainExceptionValidation.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LarTechAPi.Domain.Validations
+﻿namespace LarTechAPi.Domain.Validations
 {
     public class DomainExceptionValidation : Exception
     {
