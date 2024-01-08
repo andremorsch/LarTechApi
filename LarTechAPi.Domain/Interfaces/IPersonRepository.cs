88ba2fd1fec5e7644023c28883dde7ce19facda1
André Morsch <andremorsch37@gmail.com>
2024-01-08 20:49:59 -0300

    feat: cria interface repo Person

---
+++ LarTechAPi.Domain/Interfaces/IPersonRepository.cs
@@ -0,0 +1,13 @@
+﻿using LarTechAPi.Domain.Entities;
+
+namespace LarTechAPi.Domain.Interfaces
+{
+    public interface IPersonRepository
+    {
+        Task<IEnumerable<Person>> GetPeopleAsync();
+        Task<Person> GetPersonByIdAsync(int id);
+        Task<Person> CreatePersonAsync(Person person);
+        Task<Person> UpdatePersonAsync(Person person);
+        Task<Person> DeletePersonAsync(Person person);
+    }
+}
