e6c2bef49551723c84e418391f366995a243b287
André Morsch <andremorsch37@gmail.com>
2024-01-08 20:50:08 -0300

    feat: cria interface repo Phone

---
+++ LarTechAPi.Domain/Interfaces/IPhoneRepository.cs
@@ -0,0 +1,13 @@
+﻿using LarTechAPi.Domain.Entities;
+
+namespace LarTechAPi.Domain.Interfaces
+{
+    public interface IPhoneRepository
+    {
+        Task<IEnumerable<Phosne>> GetPhonesAsync();
+        Task<Phone> GetPhoneByIdAsync(int id);
+        Task<Phone> CreatePhoneAsync(Phone phone);
+        Task<Phone> UpdatePhoneAsync(Phone phone);
+        Task<Phone> DeletePhoneAsync(Phone phone);
+    }
+}
