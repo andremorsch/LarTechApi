5318677072f8ddfe07a43175bb010e5ba5024dee
André Morsch <andremorsch37@gmail.com>
2024-01-08 22:24:59 -0300

    feat: cria mapping DTO

---
+++ LarTechAPi.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace LarTechAPi.Application.Mappings
+{
+    public class DomainToDTOMappingProfile : Profile
+    {
+        public DomainToDTOMappingProfile()
+        {
+            CreateMap<Domain.Entities.Person, DTOs.PersonDTO>().ReverseMap();
+            CreateMap<Domain.Entities.Phone, DTOs.PhoneDTO>().ReverseMap();
+        }
+    }
+}
