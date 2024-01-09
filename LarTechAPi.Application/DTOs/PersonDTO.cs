67c75a13d1389234de1fd8d1d48403ddce51b774
André Morsch <andremorsch37@gmail.com>
2024-01-08 22:09:14 -0300

    feat: cria DTOs para as classes

---
+++ LarTechAPi.Application/DTOs/PersonDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LarTechAPi.Application.DTOs
+{
+    public class PersonDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [MinLength(3, ErrorMessage = "Name invalid")]
+        [MaxLength(50, ErrorMessage = "Name invalid")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Birthday is required")]
+        public DateTime Birthday { get; set; }
+
+        [Required(ErrorMessage = "Status is required")]
+        public bool IsActive { get; set; }
+    }
+}
---
+++ LarTechAPi.Application/DTOs/PhoneDTO.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LarTechAPi.Application.DTOs
+{
+    public class PhoneDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Phone type is required")]
+        [MinLength(3, ErrorMessage = "Phone type invalid")]
+        [MaxLength(50, ErrorMessage = "Phone type invalid")]
+        public string PhoneType { get; set; }
+
+        [Required(ErrorMessage = "Phone number is required")]
+        [MinLength(10, ErrorMessage = "Phone number invalid")]
+        [MaxLength(11, ErrorMessage = "Phone number invalid")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "PersonId is required")]
+        public int PersonId { get; set; }
+
+    }
+}
