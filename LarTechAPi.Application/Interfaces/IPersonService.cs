41d4a226a56b4f05119c6e5f7b85356d467c3463
André Morsch <andremorsch37@gmail.com>
2024-01-08 22:22:44 -0300

    feat: cria interfaces dos serviços

---
+++ LarTechAPi.Application/Interfaces/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using LarTechAPi.Application.DTOs;
+
+namespace LarTechAPi.Application.Interfaces
+{
+    public interface IPersonService
+    {
+        Task<IEnumerable<PersonDTO>> GetPeople();
+        Task<PersonDTO> GetPersonById(int? id);
+        Task<PersonDTO> CreatePerson(PersonDTO personDTO);
+        Task UpdatePerson(PersonDTO personDTO);
+        Task DeletePerson(int? id);
+    }
+}
---
+++ LarTechAPi.Application/Interfaces/IPhoneService.cs
@@ -0,0 +1,13 @@
+﻿using LarTechAPi.Application.DTOs;
+
+namespace LarTechAPi.Application.Interfaces
+{
+    public interface IPhoneService
+    {
+        Task<IEnumerable<PhoneDTO>> GetPhones();
+        Task<PhoneDTO> GetPhoneById(int? id);
+        Task<PhoneDTO> CreatePhone(PhoneDTO phoneDTO);
+        Task UpdatePhone(PhoneDTO phoneDTO);
+        Task DeletePhone(int? id);
+    }
+}
