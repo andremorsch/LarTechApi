634f3010149238f949d59d85778c27fd378abebf
André Morsch <andremorsch37@gmail.com>
2024-01-08 23:27:30 -0300

    feat: cria DbContext

---
+++ LarTechAPi.Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using LarTechAPi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LarTechAPi.Infrastructure.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+
+        public DbSet<Person> People { get; set; }
+        public DbSet<Phone> Phones { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext)
+                .Assembly);
+        }
+    }
+}
