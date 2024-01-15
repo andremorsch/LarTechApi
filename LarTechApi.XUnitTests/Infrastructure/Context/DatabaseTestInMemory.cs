d703073a7757b0358446c05d22cc71a4a566c411
André Morsch <andremorsch37@gmail.com>
2024-01-14 22:38:12 -0300

    test: cria testes para o repository de person

---
+++ LarTechApi.XUnitTests/Infrastructure/Context/DatabaseTestInMemory.cs
@@ -0,0 +1,22 @@
+﻿using LarTechAPi.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace LarTechApi.XUnitTests.Infrastructure.Context
+{
+    public class DatabaseTestInMemory
+    {
+        public static ApplicationDbContext GetDatabase()
+        {
+            var name = Guid.NewGuid().ToString();
+            return GetDatabase(name);
+        }
+
+        private static ApplicationDbContext GetDatabase(string name)
+        {
+            var inMemoryOption = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(name).Options;
+
+            return new ApplicationDbContext(inMemoryOption);
+        }
+    }
+}
---
+++ LarTechApi.XUnitTests/Infrastructure/Repositories/PersonRepositoryTest.cs
@@ -0,0 +1,93 @@
+﻿using LarTechApi.XUnitTests.Infrastructure.Context;
+using LarTechAPi.Domain.Entities;
+using LarTechAPi.Infrastructure.Context;
+using LarTechAPi.Infrastructure.Repositories;
+
+namespace LarTechApi.XUnitTests.Infrastructure.Repositories
+{
+    public class PersonRepositoryTest
+    {
+        private ApplicationDbContext _context;
+
+        [Fact(DisplayName = "Deve salvar uma pessoa no DB")]
+        public async Task Create_ShouldSaveAPerson()
+        {
+            var person = new Person("Testando ABC", "12345678910", new DateTime(1996, 01, 01), true);
+            _context = DatabaseTestInMemory.GetDatabase();
+
+            var personRepository = new PersonRepository(_context);
+            await personRepository.CreatePersonAsync(person);
+
+            var result = _context.People.FirstOrDefault();
+            Assert.Equal(person, result);
+            Assert.Equal(person.Cpf, result!.Cpf);
+
+        }
+
+        [Fact(DisplayName = "Deve retornar uma pessoa do DB pelo ID")]
+        public async Task GetByID_ShouldGetAPersonById()
+        {
+            var person = new Person("Testando ABC", "12345678910", new DateTime(1996, 01, 01), true);
+            _context = DatabaseTestInMemory.GetDatabase();
+            _context.People.Add(person);
+            _context.SaveChanges();
+
+            var personRepository = new PersonRepository(_context);
+            var result = await personRepository.GetPersonByIdAsync(1);
+
+            Assert.Equal(person, result);
+            Assert.Equal(person.Cpf, result.Cpf);
+        }
+
+        [Fact(DisplayName = "Deve Deletar uma pessoa do DB")]
+        public async Task Delete_ShouldDeleteAPerson()
+        {
+            var person = new Person("Testando ABC", "12345678910", new DateTime(1996, 01, 01), true);
+            _context = DatabaseTestInMemory.GetDatabase();
+            _context.People.Add(person);
+            _context.SaveChanges();
+
+            var personRepository = new PersonRepository(_context);
+            var resultGet = await personRepository.GetPersonByIdAsync(1);
+            var resultDelete = await personRepository.DeletePersonAsync(resultGet);
+            var result = await personRepository.GetPersonByIdAsync(1);
+
+
+            Assert.Equal(resultGet, resultDelete);
+            Assert.Equal(resultGet.Cpf, resultDelete.Cpf);
+            Assert.Null(result);
+        }
+
+        [Fact(DisplayName = "Deve retornar uma pessoa do DB pelo CPF")]
+        public async Task GetByCPF_ShouldGetPersonByCpf()
+        {
+            var person = new Person("Testando ABC", "12345678910", new DateTime(1996, 01, 01), true);
+            _context = DatabaseTestInMemory.GetDatabase();
+            _context.People.Add(person);
+            _context.SaveChanges();
+
+            var personRepository = new PersonRepository(_context);
+            var result = await personRepository.GetPersonByCpfAsync("12345678910");
+
+            Assert.Equal(person, result);
+            Assert.Equal(person.Id, result.Id);
+        }
+
+        [Fact(DisplayName = "Deve retornar todos cadastros de pessoas do DB")]
+        public async Task Update_ShouldGetPeople()
+        {
+            var person1 = new Person(1, "Testando ABC", "12345678910", new DateTime(1996, 01, 01), true);
+            var person2 = new Person(2, "Testando DEF", "01987654321", new DateTime(1969, 01, 01), false);
+            _context = DatabaseTestInMemory.GetDatabase();
+            _context.People.Add(person1);
+            _context.People.Add(person2);
+            _context.SaveChanges();
+
+            var personRepository = new PersonRepository(_context);
+            var result = await personRepository.GetPeopleAsync();
+
+            Assert.Equal(result.FirstOrDefault(x => x.Id == 1), person1);
+            Assert.Equal(result.FirstOrDefault(x => x.Id == 2), person2);
+        }
+    }
+}
